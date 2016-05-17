a0a0e0aa2458fa11c9106a9d304406fd9a7d80b9
abomb2323 <adam.crick@hotmail.com>
2016-05-16 20:18:01 -0400

    Second commit, testing some stuff to make sure things work the way they should

--- Recipe Book/src/Main.java
+++ Recipe Book/src/Main.java
@@ -1,4 +1,9 @@
-
+/*
+ * Testing the commit to github methodology wooh
+ * 
+ * Authors: Adam Crick, Emily Venuto
+ * 
+ */
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Emily, if you see this, you're golden!");
