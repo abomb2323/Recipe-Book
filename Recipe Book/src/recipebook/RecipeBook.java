bfb878bffa6a093347f84cb70e14cc4f943a89a1
abomb2323 <adam.crick@hotmail.com>
2016-05-22 01:29:11 -0400

    Version 0.0.0 - Setting Up 5/22/2016 0129
    -Removed testing files/packages
    -Added recipebook package for use with main classes
    -Added recipebook.gui package for use with the GUI
    -Inserted Javadoc tags in RecipeBook.java

---
+++ Recipe Book/src/recipebook/RecipeBook.java
@@ -0,0 +1,18 @@
+/**
+ * @author Adam Crick
+ * @author Emily Venuto
+ * 
+ * @version 0.0.0
+ * @since 5/22/2016
+ * 
+ * @description Main class for the recipe book program.
+ */
+package recipebook;
+
+public class RecipeBook {
+	
+	public static void main(String[] args){
+		
+	}
+	
+}
