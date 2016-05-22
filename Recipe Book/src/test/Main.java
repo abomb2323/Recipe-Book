5e2a5201e7004249487ea4f8f0088f29b54b1544
evenuto <emi829@verizon.net>
2016-05-21 22:13:16 -0400

    +Yasss changes

--- Recipe Book/src/test/Main.java
+++ Recipe Book/src/test/Main.java
@@ -10,7 +10,7 @@ public class Main { //This is the Main class, must have the same name as the fil
 	
 	public static void main(String[] args){ //Main method, this is what is run when the program is run **ONE PER PROGRAM**
 		
-		System.out.println("Emily, if you see this, you're golden!"); 
+		System.out.println("IHAO forever"); 
 		//Explanation: 
 		//System: This is a class containing useful things
 		//out: this is the output stuff
