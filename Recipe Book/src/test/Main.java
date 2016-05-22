78c46260edb3fe777bcda67ff37a93f852df8c54
abomb2323 <adam.crick@hotmail.com>
2016-05-21 21:26:21 -0400

    Updated for tutorial-ish lol
    This is a commit for Emily to see

---
+++ Recipe Book/src/test/Main.java
@@ -0,0 +1,20 @@
+package test; //This is a package, you can organize files into packages
+
+/*
+ * Testing the commit to github methodology wooh
+ * 
+ * Authors: Adam Crick, Emily Venuto
+ * 
+ */
+public class Main { //This is the Main class, must have the same name as the filename
+	
+	public static void main(String[] args){ //Main method, this is what is run when the program is run **ONE PER PROGRAM**
+		
+		System.out.println("Emily, if you see this, you're golden!"); 
+		//Explanation: 
+		//System: This is a class containing useful things
+		//out: this is the output stuff
+		//println: this method prints the arguments on a new line
+		System.out.println("Adding another line of code because I can");
+	}
+}
